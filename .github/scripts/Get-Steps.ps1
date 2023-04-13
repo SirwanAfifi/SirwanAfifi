97e0a07bec8d326c417b9aa1f008125810bfde14
Sirwan <sir1afifi@gmail.com>
2023-04-13 18:27:50 +0100

    better padding

--- .github/scripts/Get-Steps.ps1
+++ .github/scripts/Get-Steps.ps1
@@ -9,9 +9,9 @@ Function Set-Steps {
     $TextTags = @"
     <tspan id="step-count" font-weight="bold">$([System.String]::Format("{0:n0}", [int]$json.steps))</tspan>
 "@
-    $DatetimeTags = "<text id=""datetime"" x=""800"" y=""72"" font-size=""39"" fill=""#99989E"">$($json.date)</text>"
+    $DatetimeTags = "<text id=""datetime"" x=""710"" y=""72"" font-size=""39"" fill=""#99989E"">$($json.date)</text>"
     $SvgContent = $SvgContent -Replace '<tspan id="step-count" font-weight="bold">.*?</tspan>', $TextTags
-    $SvgContent = $SvgContent -Replace '<text id="datetime" x="800" y="72" font-size="39" fill="#99989E">.*?</text>', $DatetimeTags
+    $SvgContent = $SvgContent -Replace '<text id="datetime" x="710" y="72" font-size="39" fill="#99989E">.*?</text>', $DatetimeTags
     $SvgContent | Set-Content -Path $SvgPath
 }
 
